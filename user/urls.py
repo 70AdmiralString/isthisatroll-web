5207f33b9ac8eeecaf0b2627a516d5675cbea6aa
Roberto Albesiano <roberto.albesiano@gmail.com>
2021-03-28 18:53:17 -0400

    Create a basic redditor detail view
    Create a very basic detail view for Redditor objects in the user app. 
    Still does not handle 404 errors or new searches. The generated page is 
    not a well formed HTML page yet.

---
+++ user/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'user'
+urlpatterns = [
+    path('<str:pk>/', views.DetailView.as_view(), name='detail'),
+]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Redditor
+
+
+class DetailView(generic.DetailView):
+    """
+    The detail view for Redditors.
+
+    This class provides the view for the results of the analysis
+    on a redditor. If the redditor is not present in the database,
+    it gives the option of performing the analysis.
+    """
+
+    model = Redditor

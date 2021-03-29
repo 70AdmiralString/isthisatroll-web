91f7bd5feae4cb166477374da20eee6748fdb6bd
Roberto Albesiano <roberto.albesiano@gmail.com>
2021-03-28 22:04:15 -0400

    Provide the docstring for SearchForm

--- user/forms.py
+++ user/forms.py
@@ -1,4 +1,9 @@
 from django import forms
 
+
 class SearchForm(forms.Form):
+    """
+    A simple search form. The only entry is the username (pk of models.Redditor)
+    """
+
     username = forms.CharField(label="Username", max_length=20, min_length=3)

4ef101cdbc830b0a4324cf53cf63674ff91bb36b
Roberto Albesiano <roberto.albesiano@gmail.com>
2021-03-30 21:31:56 -0400

    Refactor the search view to use FormView
    It is more efficient to use the builtin FormView to write the simple 
    SearchView form view.

--- user/views.py
+++ user/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.utils import timezone
-from django.views import generic, View
+from django.views import generic
 
 from .models import Redditor
 from .forms import SearchForm
@@ -20,7 +20,7 @@ class DetailView(generic.DetailView):
     model = Redditor
 
 
-class SearchView(View):
+class SearchView(generic.edit.FormView):
     """
     The search page view, with form processing.
 
@@ -30,23 +30,13 @@ class SearchView(View):
 
     form_class = SearchForm
     template_name = 'user/search.html'
-    initial = {}
 
-    # if a GET request, render the search page
-    def get(self, request, *args, **kwargs):
-        form = self.form_class(initial=self.initial)
-        return render(request, self.template_name, {'form': form})
-
-    # if a POST request, process the data and redirect to detail of user
-    def post(self, request, *args, **kwargs):
-        form = SearchForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            # if the user is not already in the database, create a new entry
-            if not Redditor.objects.filter(pk=username).exists():
-                new_redditor = Redditor(username=username,
-                                        analysis_date=timezone.now(),
-                                        result=0.1)
-                new_redditor.save()
-            return HttpResponseRedirect(reverse('user:detail', args=(username,)))
-        return render(request, self.template_name, {'form': form})
+    def form_valid(self, form):
+        username = form.cleaned_data['username']
+        # if the user is not already in the database, create a new entry
+        if not Redditor.objects.filter(pk=username).exists():
+            new_redditor = Redditor(username=username,
+                                    analysis_date=timezone.now(),
+                                    result=0.1)
+            new_redditor.save()
+        return HttpResponseRedirect(reverse('user:detail', args=(username,)))

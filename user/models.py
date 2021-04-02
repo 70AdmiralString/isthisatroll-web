4b29777baed78111e77c5590b062562af3925fc7
Roberto Albesiano <roberto.albesiano@gmail.com>
2021-04-02 17:57:28 -0400

    Fix linter

--- user/models.py
+++ user/models.py
@@ -5,7 +5,7 @@ from django.core.validators import RegexValidator
 class Redditor(models.Model):
     username = models.CharField(
         primary_key=True,
-        max_length = 20,
+        max_length=20,
         validators=[RegexValidator(
             regex=r'^[A-Za-z0-9\-\_]{3,20}',
             message='Username must be between 3 and 20 alphanumeric characters, plus - and _.'
@@ -16,5 +16,5 @@ class Redditor(models.Model):
 
     def __str__(self):
         return f"""Username: {self.username}
-                 Date analyzed: {self.analysis_date}
-                 Result: {self.result}"""
+            Date analyzed: {self.analysis_date}
+            Result: {self.result}"""

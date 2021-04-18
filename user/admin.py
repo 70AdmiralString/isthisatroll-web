ebd1eb14b4db95d806bfa8340ca5eb5f119276fb
Roberto Albesiano <roberto.albesiano@gmail.com>
2021-04-18 16:30:28 -0400

    Order Redditors by analysis date in admin, add result field in display

--- user/admin.py
+++ user/admin.py
@@ -6,6 +6,9 @@ from .models import Redditor
 @admin.register(Redditor)
 class RedditorAdmin(admin.ModelAdmin):
     """ Settings for the Redditor table in admin page. """
-    list_display = ('username', 'analysis_date')
+    list_display = ('username', 'analysis_date', 'result')
     list_filter = ['analysis_date', 'result']
     search_fields = ['username']
+
+    def get_ordering(self, request):
+        return ['-analysis_date']

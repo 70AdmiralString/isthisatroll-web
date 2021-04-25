ddb05d11a6ae77f600970866fba85ee70b845eec
Roberto Albesiano <roberto.albesiano@gmail.com>
2021-04-25 11:30:23 -0400

    Add basic client-side validation for searchform

---
+++ user/static/user/js/search.js
@@ -0,0 +1,21 @@
+// Extra JavaScript code for the search template (search.html)
+
+// Form validation: overall validation
+$("form.needs-validation").validate({
+  debug: true,
+  errorClass: "is-invalid",
+  errorElement: "div",
+  errorPlacement: function(error, element) {
+    error.appendTo("#errorBox")
+  }
+});
+
+// Form validation: rules for each input element
+$("input.needs-validation").rules("add", {
+  minlength: 3,
+  maxlength: 20,
+  pattern: "^[A-Za-z0-9\-\_]{3,20}$",
+  messages: {
+    pattern: "Should match ^[A-Za-z0-9\-\_]{3,20}$"
+  }
+});

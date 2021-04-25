f24278e478333f91980b83f9a4922a512fa4916d
Roberto Albesiano <roberto.albesiano@gmail.com>
2021-04-25 15:24:37 -0400

    Add document ready check

--- user/static/user/js/search.js
+++ user/static/user/js/search.js
@@ -1,19 +1,25 @@
 // Extra JavaScript code for the search template (search.html)
 
-// Form validation: overall validation
-$("form.needs-validation").validate({
-  debug: true,
-  errorClass: "is-invalid",
-  errorElement: "div",
-  errorPlacement: function(error, element) {
-    error.appendTo("#errorBox")
-  }
-});
+$(document).ready(
+  function() {
 
-// Form validation: rules for each input element
-$("input.needs-validation").rules("add", {
-  pattern: "^[A-Za-z0-9\-\_]{3,20}$",
-  messages: {
-    pattern: "Should match ^[A-Za-z0-9\-\_]{3,20}$"
+    // Form validation: overall validation
+    $("form.needs-validation").validate({
+      debug: true,
+      errorClass: "is-invalid",
+      errorElement: "div",
+      errorPlacement: function(error, element) {
+        error.appendTo("#errorBox")
+      }
+    });
+
+    // Form validation: rules for each input element
+    $("input.needs-validation").rules("add", {
+      pattern: "^[A-Za-z0-9\-\_]{3,20}$",
+      messages: {
+        pattern: "Should match ^[A-Za-z0-9\-\_]{3,20}$"
+      }
+    });
+
   }
-});
+)
